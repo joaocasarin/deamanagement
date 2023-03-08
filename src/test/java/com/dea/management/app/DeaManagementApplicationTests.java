ff1e4690a5a3f290840957b1bff2c03e42f57e46
Joao Vitor Casarin <joaovitorcasarin@hotmail.com>
2023-03-08 10:36:29 -0300

    test: add automated tests

--- src/test/java/com/dea/management/app/DeaManagementApplicationTests.java
+++ src/test/java/com/dea/management/app/DeaManagementApplicationTests.java
@@ -1,9 +1,13 @@
 package com.dea.management.app;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DeaManagementApplicationTests {
 
 	@Test
--- src/test/java/com/dea/management/app/HelloWorldControllerTest.java
+++ src/test/java/com/dea/management/app/HelloWorldControllerTest.java
@@ -1,10 +1,12 @@
 package com.dea.management.app;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -13,6 +15,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutoConfigureMockMvc
 public class HelloWorldControllerTest {
 
